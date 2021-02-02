45253c89c62d93b3d7c3b128da799c316a048403
unknown <degnexo@gmail.com>
2021-02-02 20:14:27 -0300

    #7 added validations to user Create/edit fields

--- src/components/usercrud/UserCreate.js
+++ src/components/usercrud/UserCreate.js
@@ -1,13 +1,52 @@
 import * as React from "react";
-import { Create, SimpleForm, TextInput, BooleanInput, PasswordInput,required } from 'react-admin';
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  BooleanInput,
+  PasswordInput,
+  required,
+} from "react-admin";
+
+const matchPassword = (value, allValues) => {
+  if (value !== allValues.password) {
+    return `Las contraseñas no coinciden`;
+  }
+};
+
+const validateEmail = (value) => {
+  let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (reg.test(value) === false) {
+    return "Email incorrecto";
+  }
+};
+
+const minLength = (min) => (value) => {
+  if (value && value.length < min) {
+    return "Debe tener mas de " + min + " caracteres";
+  }
+};
 
 export const UserCreate = (props) => (
-    <Create  title="Creando Usuario" {...props}>
-        <SimpleForm>
-            <TextInput source="name" validate={[required()]}/>
-            <TextInput source="email" validate={[required()]} />
-            <PasswordInput source="password" validate={[required()]}/>
-            <BooleanInput source="superadmin" />
-        </SimpleForm>
-    </Create>
+  <Create title="Creando Usuario" {...props}>
+    <SimpleForm>
+      <TextInput
+        source="name"
+        label="nombre"
+        validate={[required(), minLength(3)]}
+      />
+      <TextInput source="email" validate={[required(), validateEmail]} />
+      <PasswordInput
+        source="password"
+        label="contraseña"
+        validate={[required(), minLength(7)]}
+      />
+      <PasswordInput
+        source="cpassword"
+        label="confirmación"
+        validate={[required(), matchPassword]}
+      />
+      <BooleanInput source="superadmin" />
+    </SimpleForm>
+  </Create>
 );
--- src/components/usercrud/UserEdit.js
+++ src/components/usercrud/UserEdit.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { Edit, SimpleForm, TextInput, BooleanInput, required } from 'react-admin';
 
+const minLength = (min) => (value) => {
+    if (value && value.length < min) {
+      return "Debe tener mas de " + min + " caracteres";
+    }
+  };
+
 export const UserEdit = (props) => (
     <Edit title="Editando Usuario" {...props}>
         <SimpleForm >
-            <TextInput source="name" validate={[required()]}/>
+            <TextInput source="name" validate={[required(), minLength(3)]}/>
             <TextInput source="email"  validate={[required()]}/>
             <BooleanInput source="superadmin" />
         </SimpleForm>
