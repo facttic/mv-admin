02834d3543d99ef979d0f9fa847e185c3f6f2a20
unknown <degnexo@gmail.com>
2021-02-03 16:01:03 -0300

    #7 bulk delete and export removed

--- src/components/usercrud/UserList.js
+++ src/components/usercrud/UserList.js
@@ -2,8 +2,8 @@ import * as React from "react";
 import { List, Datagrid, TextField, BooleanField, DeleteButton} from 'react-admin';
 
 export const UserList = props => (
-    <List title="Usuarios"  {...props}>
-        <Datagrid rowClick="edit">
+    <List title="Usuarios" exporter={false} bulkActionButtons={false} {...props}>
+        <Datagrid rowClick="edit" >
             <TextField source="name" label="Nombre"/>
             <TextField source="email" label="E-mail"/>
             <BooleanField source="superadmin" />
